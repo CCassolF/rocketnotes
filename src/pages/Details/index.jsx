fa975fb757b22bed7c1f52be8fa85e0cf56d699f
carlos <carloscassolfilho@gmail.com>
2023-08-03 15:28:22 -0300

    setting global styles

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -6,17 +6,6 @@ export function Details() {
       <h1>Rocket Notes</h1>
       <span>Aplicações para salvar e gerenciar seus links úteis.</span>
 
-
-      <h2>Faça seu login</h2>
-
-      <section>
-        <input type="email" placeholder="E-mail"/>
-        <input type="password" placeholder="Senha"/>
-      </section>
-
-      <button>Entrar</button>
-
-      <p>Criar conta</p>
     </Container>
   )
 }
