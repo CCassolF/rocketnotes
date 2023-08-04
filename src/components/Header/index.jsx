c392473f75143ffc2619dd73d399ea252d3e8338
carlos <carloscassolfilho@gmail.com>
2023-08-04 14:43:02 -0300

    Component: Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,24 @@
+import { RiShutDownLine } from 'react-icons/ri';
+import { Container, Logout, Profile } from "./styles";
+
+export function Header() {
+  return (
+    <Container>
+      <Profile>
+        <img 
+          src="https://github.com/ccassolf.png" 
+          alt="Foto do usuário." 
+        />
+
+        <div>
+          <span>Bem-vindo</span>
+          <strong>Carlos Cassol</strong>
+        </div>
+      </Profile>
+
+      <Logout>
+        <RiShutDownLine />
+      </Logout>
+    </Container>
+  )
+}
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,15 +1,12 @@
 import { Button } from "../../components/Button/index.jsx"
+import { Header } from "../../components/Header/index.jsx"
 import { Container } from "./styles.js"
 
 export function Details() {
   return (
     <Container>
-      <h1>Rocket Notes</h1>
-      <span>Aplicações para salvar e gerenciar seus links úteis.</span>
-
-      <Button title="Login" loading />
-      <Button title="cadastrar" />
-      <Button title="Voltar" />
+      <Header />
+      <Button title="Login" />
     </Container>
   )
 }
