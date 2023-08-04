1667e0b08bdb44cabd639753bca68e59e44509b0
carlos <carloscassolfilho@gmail.com>
2023-08-04 14:54:55 -0300

    Component: Section

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,11 +1,24 @@
 import { Button } from "../../components/Button/index.jsx"
 import { Header } from "../../components/Header/index.jsx"
-import { Container } from "./styles.js"
+import { Section } from "../../components/Section/index.jsx"
+import { Container, Links } from "./styles.js"
 
 export function Details() {
   return (
     <Container>
       <Header />
+
+      <Section title="Links úteis">
+        <Links>
+          <li>
+            <a href="#">https://www.rocketseat.com.br/</a>
+          </li>
+          <li>
+            <a href="#">https://www.rocketseat.com.br/</a>
+          </li>
+        </Links>
+      </Section>
+
       <Button title="Login" />
     </Container>
   )
