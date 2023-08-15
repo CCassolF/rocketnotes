25e8761d6b634b9afadf37a03c6c6488737580d2
carlos <carloscassolfilho@gmail.com>
2023-08-14 23:43:48 -0300

    Update user profile

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -35,6 +35,25 @@ function AuthProvider({ children }) {
     setData({})
   }
 
+  async function updateProfile({ user }) {
+    try {
+
+      await api.put('/users', user)
+      localStorage.setItem("@rocketnotes:user", JSON.stringify(user))
+
+      setData({ user, token: data.token })
+
+      alert('Perfil atualizado!')
+
+    } catch (error) {
+      if(error.response) {
+        alert(error.response.data.message)
+      } else {
+        alert("Não foi possível atualizar o perfil.")
+      }
+    }
+  }
+
   useEffect(() => {
     const token = localStorage.getItem("@rocketnotes:token")
     const user = localStorage.getItem("@rocketnotes:user")
@@ -53,6 +72,7 @@ function AuthProvider({ children }) {
     <AuthContext.Provider value={{ 
       signIn, 
       signOut,
+      updateProfile,
       user: data.user 
       }}
     >
--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -3,9 +3,30 @@ import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
 import { Avatar, Container, Form } from "./styles";
 
+import { useAuth } from '../../hooks/auth';
+
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export function Profile() {
+  const { user, updateProfile } = useAuth()
+
+  const [name, setName] = useState(user.name)
+  const [email, setEmail] = useState(user.email)
+  const [passwordOld, setPasswordOld] = useState()
+  const [passwordNew, setPasswordNew] = useState()
+
+  async function handleUpdate() {
+    const user = {
+      name,
+      email,
+      password: passwordNew,
+      old_password: passwordOld
+    }
+
+    await updateProfile({ user })
+  }
+
   return (
     <Container>
       <header>
@@ -34,27 +55,33 @@ export function Profile() {
           placeholder="Nome"
           type="text"
           icon={FiUser}
+          value={name}
+          onChange={e => setName(e.target.value)}
         />
 
         <Input 
           placeholder="E-mail"
           type="text"
           icon={FiMail}
+          value={email}
+          onChange={e => setEmail(e.target.value)}
         />
 
         <Input 
           placeholder="Senha atual"
           type="password"
           icon={FiLock}
+          onChange={e => setPasswordOld(e.target.value)}
         />
 
         <Input 
           placeholder="Nova senha"
           type="password"
           icon={FiLock}
+          onChange={e => setPasswordNew(e.target.value)}
         />
 
-        <Button title="Salvar" />
+        <Button title="Salvar" onClick={handleUpdate} />
       </Form>
     </Container>
   )
