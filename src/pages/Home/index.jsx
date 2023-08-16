3cfa5ee4d8589b6b123c27516f8e841e454010e0
carlos <carloscassolfilho@gmail.com>
2023-08-16 12:19:40 -0300

    tag selected

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,41 @@
 import { FiPlus } from 'react-icons/fi'
 import { Brand, Container, Content, Menu, NewNote, Search } from './styles'
 
+import { useEffect, useState } from 'react'
 import { ButtonText } from '../../components/ButtonText'
 import { Header } from '../../components/Header'
 import { Input } from '../../components/Input'
 import { Note } from '../../components/Note'
 import { Section } from '../../components/Section'
 
+import { api } from '../../services/api'
+
 export function Home() {
+  const [tags, setTags] = useState([])
+  const [tagsSelected, setTagsSelected] = useState([])
+
+  function handleTagsSelected(tagName) {
+    const alreadySelected = tagsSelected.includes(tagName)
+    
+    if(alreadySelected) {
+      const filteredTags = tagsSelected.filter(tag => tag !== tagName)
+      setTagsSelected(filteredTags)
+      
+    } else {
+      setTagsSelected(prevState => [...prevState, tagName])
+    }
+    
+  }
+
+  useEffect(() => {
+    async function fetchTags() {
+      const response = await api.get('/tags')
+      setTags(response.data)
+    }
+
+    fetchTags()
+  },[])
+
   return (
     <Container>
       <Brand>
@@ -18,14 +46,24 @@ export function Home() {
       
       <Menu>
         <li>
-          <ButtonText title="Todos" isActive />
+          <ButtonText 
+            title="Todos" 
+            onClick={() => handleTagsSelected("all")}
+            isActive={tagsSelected.length === 0} 
+          />
         </li>
-        <li>
-          <ButtonText title="React" />
-        </li>
-        <li>
-          <ButtonText title="Nodejs" />
-        </li>
+        {
+          tags && tags.map((tag) => (
+            <li key={String(tag.id)}>
+              <ButtonText 
+                title={tag.name} 
+                onClick={() => handleTagsSelected(tag.name)}
+                isActive={tagsSelected.includes(tag.name)} 
+              />
+            </li>
+          ))
+        }
+        
       </Menu>
 
       <Search>
