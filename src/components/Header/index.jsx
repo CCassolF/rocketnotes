77b5370d910321f21d99ea5c47332dc2a7ca73fd
carlos <carloscassolfilho@gmail.com>
2023-08-17 11:38:41 -0300

    fix avatarPlaceholder

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,9 @@
 import { RiShutDownLine } from 'react-icons/ri';
-import { useAuth } from '../../hooks/auth';
-import { Container, Logout, Profile } from "./styles";
-
 import { useNavigate } from 'react-router-dom';
+import avatarPlaceholder from '../../assets/avatar_placeholder.svg';
+import { useAuth } from '../../hooks/auth';
 import { api } from '../../services/api';
+import { Container, Logout, Profile } from "./styles";
 
 export function Header() {
   const { signOut, user } = useAuth()
